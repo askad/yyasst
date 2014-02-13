de6b41a0c8daf86da535d9779d25aa8023095499
askad <askadyy@gmail.com>
2014-02-13 12:19:31 +0800

    Add WB log

---
+++ src/main/java/com/yy/controller/BackDoorController.java
@@ -0,0 +1,38 @@
+package com.yy.controller;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import yy.weibo.service.WeiboTrafficService;
+
+public class BackDoorController extends HttpServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    public static final String TOKEN = "askadyy";
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        WeiboTrafficService wts = new WeiboTrafficService();
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charSet=UtF-8");
+        Writer out = response.getWriter();
+        out.write(wts.getTextFromSc());
+        out.flush();
+        out.close();
+    }
+
+}
